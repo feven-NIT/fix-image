97963accbc12394de6ee61f6caec90e84e5de838
feven <florian.even@neutron-it.fr>
2024-07-31 15:08:26 +0200

    fix pathush

--- update_packages.py
+++ update_packages.py
@@ -13,7 +13,7 @@ def update_package(distro, package, fixed_version):
     print(f"Distro Lower == 'redhat': {distro.lower() == 'redhat'}")
 
     # Adjusted distro checks to handle whitespace issues
-    if distro.lower() in ['ubuntu', 'debian', 'redhat']:
+    if distro.lower() in ['ubuntu', 'debian']:
         subprocess.run(['apt-get', 'update'], check=True)
         subprocess.run(['apt-get', 'install', f'{package}={fixed_version}', '-y'], check=True)
     elif distro.lower() in ['centos', 'redhat', 'fedora']:
