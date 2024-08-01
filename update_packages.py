820b902d1f73dc7a768703280a595f8c6cb2d57e
feven <florian.even@neutron-it.fr>
2024-08-01 14:34:16 +0200

    fix pathushush

--- update_packages.py
+++ update_packages.py
@@ -1,17 +1,18 @@
 import os
 import csv
-import json
 import subprocess
 
 def install_python_with_pip():
     # Install Python and pip using yum
     print("Installing Python and pip...")
     subprocess.run(['yum', 'install', '-y', 'python3-pip'], check=True)
+    print("Successfully installed Python and pip.")
 
 def remove_python_with_pip():
     # Remove Python and pip using yum
     print("Removing Python and pip...")
     subprocess.run(['yum', 'remove', '-y', 'python3-pip'], check=True)
+    print("Successfully removed Python and pip.")
 
 def update_package(distro, package, fixed_version):
     # Remove leading and trailing whitespace from distro
@@ -25,17 +26,8 @@ def update_package(distro, package, fixed_version):
     command = ['pip3', 'install', f'{package}=={fixed_version}']
     print(f"Executing command: {' '.join(command)}")
     subprocess.run(command, check=True)
+    print(f"Successfully updated {package} to version {fixed_version}.")
     remove_python_with_pip()
-
-def get_package_type(package_name):
-    with open('/tmp/tpackage-json.txt', 'r') as file:
-        data = json.load(file)
-        for repo, content in data.items():
-            for pkg_group in content['packages']:
-                for pkg in pkg_group['pkgs']:
-                    if pkg['name'] == package_name:
-                        return pkg_group['pkgsType']
-    return None
 
 def main():
     csv_file = '/tmp/scan.csv'
@@ -44,11 +36,7 @@ def main():
         for row in csv_reader:
             if row['Status'].startswith('fixed in'):
                 fixed_version = row['Status'].split(' ')[-1]
-                package_type = get_package_type(row['Package'])
-                if package_type and package_type.lower() == 'python':
-                    update_package(row['Distro'], row['Package'], fixed_version)
-                else:
-                    print(f"Package type for {row['Package']} not handled by this script.")
+                update_package(row['Distro'], row['Package'], fixed_version)
             elif row['Status'] == 'affected':
                 print(f"Package {row['Package']} in {row['Distro']} is affected by {row['CVE ID']} but no fixed version specified.")
 
